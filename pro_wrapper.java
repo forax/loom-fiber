2bca8829f1cf0934cb64b6ef0d10f7b17e7e359d
forax <forax@univ-mlv.fr>
2019-05-11 23:22:46 +0200

    update to latest pro_wrapper

--- pro_wrapper.java
+++ pro_wrapper.java
@@ -3,29 +3,37 @@ import static java.lang.System.getProperty;
 import static java.nio.file.Files.createDirectories;
 import static java.nio.file.Files.delete;
 import static java.nio.file.Files.exists;
+import static java.nio.file.Files.getLastModifiedTime;
 import static java.nio.file.Files.getPosixFilePermissions;
 import static java.nio.file.Files.list;
 import static java.nio.file.Files.newBufferedReader;
 import static java.nio.file.Files.newOutputStream;
 import static java.nio.file.Files.readAllLines;
 import static java.nio.file.Files.setPosixFilePermissions;
+import static java.nio.file.Files.walk;
 import static java.nio.file.Files.walkFileTree;
 import static java.nio.file.Files.write;
 import static java.nio.file.attribute.PosixFilePermission.GROUP_EXECUTE;
 import static java.nio.file.attribute.PosixFilePermission.OTHERS_EXECUTE;
 import static java.nio.file.attribute.PosixFilePermission.OWNER_EXECUTE;
+import static java.util.Collections.reverseOrder;
+import static java.util.Comparator.comparing;
 import static java.util.function.Predicate.not;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.UncheckedIOException;
+import java.net.InetAddress;
 import java.net.URL;
+import java.net.UnknownHostException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.FileVisitResult;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.SimpleFileVisitor;
 import java.nio.file.attribute.BasicFileAttributes;
+import java.nio.file.attribute.FileTime;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
@@ -77,14 +85,40 @@ class pro_wrapper {
     return System.getProperty("user.home");
   }
   
-  private static Optional<String> lastestReleaseVersion() throws IOException {
+  private static Optional<String> lastestReleaseVersionFromGitHub() throws IOException {
+    System.out.println("try to find latest release on Github ...");
     var url = new URL(GITHUB_API_RELEASES);
+    try {
+      InetAddress.getByName(url.getHost());  // is internet accessible ?
+    } catch(@SuppressWarnings("unused") UnknownHostException e) {
+      return Optional.empty();
+    }
     try(var input = url.openStream();
         var reader = new InputStreamReader(input, StandardCharsets.UTF_8);
         var buffered = new BufferedReader(reader, 8192)) {
       return buffered.lines()
         .flatMap(line -> Optional.of(PATTERN.matcher(line)).filter(Matcher::find).map(matcher -> matcher.group(1)).stream())
         .findFirst();
+    }
+  }
+  
+  private static FileTime lastModified(Path path) {
+    try {
+      return getLastModifiedTime(path);
+    } catch (IOException e) {
+      throw new UncheckedIOException(e);
+    }
+  }
+  
+  private static Optional<String> latestReleaseVersionFromCache(Path cache, String filename) {
+    System.out.println("try to find latest release in the cache ...");
+    try {
+      return walk(cache)
+          .filter(p -> p.getFileName().toString().equals(filename))
+          .sorted(reverseOrder(comparing(p -> lastModified(p))))
+          .findFirst().map(p -> p.getParent().getFileName().toString());
+    } catch (@SuppressWarnings("unused") IOException | UncheckedIOException e) {
+      return Optional.empty();
     }
   }
   
@@ -209,12 +243,18 @@ class pro_wrapper {
   }
   
   private static int installAndRun(String[] args) throws IOException {
-    var release = lastestReleaseVersion().orElseThrow(() -> new IOException("latest release not found on Github"));
     var specialBuild = specialBuild().map(build -> '-' + build).orElse("");
     var filename = "pro-" + platform() + specialBuild + ".zip";
+    
+    var cache = Path.of(userHome(), ".pro", "cache");
+    var release = lastestReleaseVersionFromGitHub()
+        .or(() -> latestReleaseVersionFromCache(cache, filename))
+        .orElseThrow(() -> new IOException("no release found !"));
+    
+    
     System.out.println("require " + filename + " release " + release + " ...");
     
-    var cachePath = Path.of(userHome(), ".pro", "cache", release, filename);
+    var cachePath = cache.resolve(Path.of(release, filename));
     if (!exists(cachePath)) {
       retry(cachePath, 3, _cachedPath -> download(release, filename, _cachedPath));
     }
