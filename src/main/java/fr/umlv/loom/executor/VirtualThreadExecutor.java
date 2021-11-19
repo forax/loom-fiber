1851be677107f8e55cf69c135d9fce05e7a55034
forax <forax@univ-mlv.fr>
2021-11-19 18:59:55 +0100

    re-implement ExecutorService on top of StructuredExecutor

---
+++ src/main/java/fr/umlv/loom/executor/VirtualThreadExecutor.java
@@ -0,0 +1,220 @@
+package fr.umlv.loom.executor;
+
+import java.lang.invoke.MethodHandles;
+import java.lang.invoke.VarHandle;
+import java.time.Duration;
+import java.time.Instant;
+import java.util.Collection;
+import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.RejectedExecutionException;
+import java.util.concurrent.StructuredExecutor;
+import java.util.concurrent.StructuredExecutor.ShutdownOnSuccess;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+
+public final class VirtualThreadExecutor implements ExecutorService {
+  private static final VarHandle STATE;
+  static {
+    var lookup = MethodHandles.lookup();
+    try {
+      STATE = lookup.findVarHandle(VirtualThreadExecutor.class, "state", int.class);
+    } catch (NoSuchFieldException | IllegalAccessException e) {
+      throw new AssertionError(e);
+    }
+  }
+
+  private final StructuredExecutor executor;
+  private final ExecutorService executorOfExecutor = Executors.newSingleThreadExecutor();
+  private volatile int state;
+
+  private static final int RUNNING    = 0;
+  private static final int SHUTDOWN   = 1;
+  private static final int TERMINATED = 2;
+
+  public VirtualThreadExecutor() {
+    executor = postSync(StructuredExecutor::open);
+  }
+
+  private void checkShutdownState() {
+    if (state >= SHUTDOWN) {
+      throw new RejectedExecutionException();
+    }
+  }
+
+  @SuppressWarnings("unchecked")   // very wrong but works
+  private static <T extends Throwable> AssertionError rethrow(Throwable cause) throws T {
+    throw (T) cause;
+  }
+
+  private void postAsync(Runnable runnable) {
+    executorOfExecutor.execute(runnable);
+  }
+
+  private <V> V postSync(Callable<V> callable)   {
+    var future = executorOfExecutor.submit(callable);
+    try {
+      return future.get();
+    } catch (ExecutionException e) {
+      var cause = e.getCause();
+      throw rethrow(cause);
+    } catch (InterruptedException e) {
+      throw rethrow(e);
+    }
+  }
+
+  @Override
+  public void shutdown() {
+    if (STATE.compareAndSet(this, RUNNING, SHUTDOWN)) {
+      postAsync(() -> {
+        if (state == TERMINATED) {
+          return;
+        }
+        try {
+          executor.join();
+        } catch (InterruptedException e) {
+          throw new AssertionError(e);
+        }
+        state = TERMINATED;
+        executor.close();
+        executorOfExecutor.shutdown();
+      });
+    }
+  }
+
+  @Override
+  public List<Runnable> shutdownNow() {
+    if (STATE.compareAndSet(this, RUNNING, SHUTDOWN)) {
+      postSync(() -> {
+        if (state == TERMINATED) {
+          return null;
+        }
+        executor.shutdown();
+        executor.join();
+        state = TERMINATED;
+        executor.close();
+        executorOfExecutor.shutdown();
+        return null;
+      });
+    }
+    return List.of();
+  }
+
+  @Override
+  public boolean isShutdown() {
+    return state >= SHUTDOWN;
+  }
+
+  @Override
+  public boolean isTerminated() {
+    return state == TERMINATED;
+  }
+
+  @Override
+  public boolean awaitTermination(long timeout, TimeUnit unit) {
+    if (state == TERMINATED) {
+      return false;
+    }
+    var deadline = Instant.now().plus(Duration.ofNanos(unit.toNanos(timeout)));
+    return postSync(() -> {
+      if (state == TERMINATED) {
+        return false;
+      }
+      var result = true;
+      try {
+        executor.joinUntil(deadline);
+      } catch (TimeoutException e) {
+        result = false;
+      }
+      executor.shutdown();
+      state = TERMINATED;
+      executor.close();
+      executorOfExecutor.shutdown();
+      return result;
+    });
+  }
+
+  @Override
+  public void execute(Runnable command) {
+    submit(command);
+  }
+
+  @Override
+  public <T> Future<T> submit(Callable<T> task) {
+    checkShutdownState();
+    return postSync(() -> executor.fork(task));
+  }
+
+  @Override
+  public <T> Future<T> submit(Runnable task, T result) {
+    return submit(() -> {
+      task.run();
+      return result;
+    });
+  }
+
+  @Override
+  public Future<?> submit(Runnable task) {
+    return submit(task, null);
+  }
+
+  @Override
+  public <T> List<Future<T>> invokeAll(Collection<? extends Callable<T>> tasks) throws InterruptedException {
+    checkShutdownState();
+    return postSync(() -> {
+      try(var executor = StructuredExecutor.open()) {
+        var list = tasks.stream().map(executor::fork).toList();
+        executor.join();
+        return list;
+      }
+    });
+  }
+
+  @Override
+  public <T> List<Future<T>> invokeAll(Collection<? extends Callable<T>> tasks, long timeout, TimeUnit unit) throws InterruptedException {
+    checkShutdownState();
+    var deadline = Instant.now().plus(Duration.ofNanos(unit.toNanos(timeout)));
+    return postSync(() -> {
+      try(var executor = StructuredExecutor.open()) {
+        var futures = tasks.stream().map(executor::fork).toList();
+        try {
+          executor.joinUntil(deadline);
+        } catch(TimeoutException e) {
+          return futures.stream().filter(Future::isDone).toList();
+        }
+        return futures;
+      }
+    });
+  }
+
+  @Override
+  public <T> T invokeAny(Collection<? extends Callable<T>> tasks) throws InterruptedException, ExecutionException {
+    checkShutdownState();
+    return postSync(() -> {
+      try(var executor = StructuredExecutor.open()) {
+        var handler = new ShutdownOnSuccess<T>();
+        tasks.forEach(callable -> executor.fork(callable, handler));
+        executor.join();
+        return handler.result();
+      }
+    });
+  }
+
+  @Override
+  public <T> T invokeAny(Collection<? extends Callable<T>> tasks, long timeout, TimeUnit unit) throws InterruptedException, ExecutionException, TimeoutException {
+    checkShutdownState();
+    var deadline = Instant.now().plus(Duration.ofNanos(unit.toNanos(timeout)));
+    return postSync(() -> {
+      try(var executor = StructuredExecutor.open()) {
+        var handler = new ShutdownOnSuccess<T>();
+        tasks.forEach(callable -> executor.fork(callable, handler));
+        executor.joinUntil(deadline);
+        return handler.result();
+      }
+    });
+  }
+}
---
+++ src/test/java/fr/umlv/loom/executor/VirtualThreadExecutorTest.java
@@ -0,0 +1,214 @@
+package fr.umlv.loom.executor;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class VirtualThreadExecutorTest {
+
+  @Test
+  public void shutdown() throws ExecutionException, InterruptedException {
+    var executor = new VirtualThreadExecutor();
+    var future = executor.submit(() -> {
+      Thread.sleep(100);
+      return "hello";
+    });
+    executor.shutdown();
+    assertEquals("hello", future.get());
+  }
+
+  @Test
+  public void shutdownNow() throws InterruptedException {
+    var executor = new VirtualThreadExecutor();
+    var future = executor.submit(() -> {
+      Thread.sleep(1_000);
+      return "hello";
+    });
+    executor.shutdownNow();
+    try {
+      future.get();
+      fail();
+    } catch (ExecutionException e) {
+      assertTrue(e.getCause() instanceof InterruptedException);
+    }
+  }
+
+  @Test
+  public void isShutdown() {
+    var executor = new VirtualThreadExecutor();
+    executor.shutdown();
+    assertTrue(executor.isShutdown());
+  }
+
+  @Test
+  public void isTerminated() {
+    var executor = new VirtualThreadExecutor();
+    executor.awaitTermination(1, TimeUnit.SECONDS);
+    assertTrue(executor.isTerminated());
+  }
+
+  @Test
+  public void awaitTermination() {
+    var executor = new VirtualThreadExecutor();
+    var future = executor.submit(() -> {
+      Thread.sleep(1);
+      return "hello";
+    });
+    executor.awaitTermination(1, TimeUnit.SECONDS);
+    assertEquals("hello", future.resultNow());
+  }
+
+  @Test
+  public void execute() {
+    var executor = new VirtualThreadExecutor();
+    var result = new AtomicBoolean();
+    executor.execute(() -> result.set(true));
+    executor.awaitTermination(1, TimeUnit.SECONDS);
+    assertTrue(result.get());
+  }
+
+  @Test
+  public void submitCallableResult() throws ExecutionException, InterruptedException {
+    var executor = new VirtualThreadExecutor();
+    var future = executor.submit(() -> {
+      Thread.sleep(50);
+      return 42;
+    });
+    executor.shutdown();
+    assertEquals(42, (int) future.get());
+  }
+  @Test
+  public void submitCallableException() throws InterruptedException {
+    var executor = new VirtualThreadExecutor();
+    var future = executor.submit(() -> {
+      Thread.sleep(50);
+      throw new RuntimeException("oops");
+    });
+    executor.shutdown();
+    try {
+      future.get();
+      fail();
+    } catch(ExecutionException e) {
+      assertTrue(e.getCause() instanceof RuntimeException);
+    }
+  }
+
+  @Test
+  public void testSubmitRunnable() throws ExecutionException, InterruptedException {
+    var executor = new VirtualThreadExecutor();
+    var result = new AtomicBoolean();
+    var future = executor.submit((Runnable) () -> result.set(true));
+    executor.shutdown();
+    assertNull(future.get());
+    assertTrue(result.get());
+  }
+
+  @Test
+  public void testSubmitRunnableWithValue() throws ExecutionException, InterruptedException {
+    var executor = new VirtualThreadExecutor();
+    var result = new AtomicBoolean();
+    var future = executor.submit((Runnable) () -> result.set(true), 42);
+    executor.shutdown();
+    assertEquals(42, (int) future.get());
+    assertTrue(result.get());
+  }
+
+  @Test
+  public void invokeAll() throws InterruptedException {
+    var executor = new VirtualThreadExecutor();
+    var tasks = List.<Callable<Integer>>of(
+        () -> {
+          Thread.sleep(1_000);
+          return 1;
+        },
+        () -> {
+          Thread.sleep(1_000);
+          return 101;
+        });
+    var list = executor.invokeAll(tasks);
+    executor.shutdown();
+    assertEquals(List.of(1, 101), list.stream().map(Future::resultNow).toList());
+  }
+
+  @Test
+  public void invokeAllWithTimeout() throws InterruptedException {
+    var executor = new VirtualThreadExecutor();
+    var tasks = List.<Callable<Integer>>of(
+        () -> 1,
+        () -> 101
+    );
+    var list = executor.invokeAll(tasks, 1, TimeUnit.SECONDS);
+    executor.shutdown();
+    assertEquals(List.of(1, 101), list.stream().map(Future::resultNow).toList());
+  }
+  @Test
+  public void invokeAllWithTimeoutDeadlineReached() throws InterruptedException {
+    var executor = new VirtualThreadExecutor();
+    var tasks = List.<Callable<Integer>>of(
+        () -> {
+          Thread.sleep(1_000);
+          return 1;
+        },
+        () -> {
+          return 101;
+        });
+    var list = executor.invokeAll(tasks, 500, TimeUnit.MILLISECONDS);
+    executor.shutdown();
+    assertEquals(List.of(101), list.stream().map(Future::resultNow).toList());
+  }
+
+  @Test
+  public void invokeAny() throws ExecutionException, InterruptedException, TimeoutException {
+    var executor = new VirtualThreadExecutor();
+    var tasks = List.<Callable<Integer>>of(
+        () -> {
+          return 42;
+        },
+        () -> {
+          Thread.sleep(1_000);
+          return 101;
+        });
+    var result = executor.invokeAny(tasks);
+    executor.shutdown();
+    assertEquals(42, (int) result);
+  }
+
+  @Test
+  public void invokeAnyWithTimeout() throws ExecutionException, InterruptedException, TimeoutException {
+    var executor = new VirtualThreadExecutor();
+    var tasks = List.<Callable<Integer>>of(
+        () -> {
+          Thread.sleep(50);
+          return 42;
+        },
+        () -> {
+          return 101;
+        });
+    var result = executor.invokeAny(tasks, 1_000, TimeUnit.MILLISECONDS);
+    executor.shutdown();
+    assertEquals(101, (int) result);
+  }
+  @Test
+  public void invokeAnyWithTimeoutDeadlineReached() throws ExecutionException, InterruptedException, TimeoutException {
+    var executor = new VirtualThreadExecutor();
+    var tasks = List.<Callable<Integer>>of(
+        () -> {
+          Thread.sleep(1_000);
+          return 42;
+        },
+        () -> {
+          Thread.sleep(1_000);
+          return 101;
+        });
+    assertThrows(TimeoutException.class, () -> executor.invokeAny(tasks, 50, TimeUnit.MILLISECONDS));
+    executor.shutdown();
+  }
+}
