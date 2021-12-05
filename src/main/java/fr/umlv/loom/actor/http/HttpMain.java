1200c029eff559ea6e1bede1abd668b5a79c0173
forax <forax@univ-mlv.fr>
2021-12-06 00:58:00 +0100

    simplify the code by not having an interface

--- src/main/java/fr/umlv/loom/actor/http/HttpMain.java
+++ src/main/java/fr/umlv/loom/actor/http/HttpMain.java
@@ -1,6 +1,7 @@
 package fr.umlv.loom.actor.http;
 
 import fr.umlv.loom.actor.Actor;
+import fr.umlv.loom.actor.Actor.Context;
 import fr.umlv.loom.actor.http.HttpServer.PathVariable;
 import fr.umlv.loom.actor.http.HttpServer.RequestBody;
 import fr.umlv.loom.actor.http.HttpServer.RequestMapping;
@@ -17,54 +18,47 @@ import static fr.umlv.loom.actor.http.HttpServer.RequestMethod.DELETE;
 import static fr.umlv.loom.actor.http.HttpServer.RequestMethod.POST;
 
 public class HttpMain {
-  public static void main(String[] args) throws IOException, InterruptedException {
-    record Task(long id, String content) {}
-    record TaskWithoutId(String content) {}
+  record Task(long id, String content) {}
+  record TaskWithoutId(String content) {}
 
-
-    interface TaskController {
-      @RequestMapping(path = "/tasks")
-      void getAllTasks(Actor<Response<List<Task>>> reply);
-
-      @RequestMapping(path = "/tasks", method = POST)
-      void createTask(@RequestBody TaskWithoutId user, Actor<Response<Task>> reply);
-
-      @RequestMapping(path = "/tasks/{id}", method = DELETE)
-      void deleteTask(@PathVariable("id") String id, Actor<Response<Void>> reply);
+  record TaskController(Context context, List<Task> tasks) {
+    @RequestMapping(path = "/tasks")
+    public void getAllTasks(Actor<Response<List<Task>>> reply) {
+      System.err.println("getAllTasks");
+      context.postTo(reply, $ -> $.response(OK, tasks));
     }
+
+    @RequestMapping(path = "/tasks", method = POST)
+    public void createTask(@RequestBody TaskWithoutId taskWithoutId, Actor<Response<Task>> reply) {
+      System.err.println("createTask " + taskWithoutId);
+      var task = new Task(tasks.size(), taskWithoutId.content);
+      tasks.add(task);
+      context.postTo(reply, $ -> $.response(OK, task));
+    }
+
+    @RequestMapping(path = "/tasks/{id}", method = DELETE)
+    public void deleteTask(@PathVariable("id") String id, Actor<Response<Void>> reply) {
+      System.err.println("deleteTask " + id);
+      var taskId = Integer.parseInt(id);
+      var removed = tasks.removeIf(task -> task.id == taskId);
+      if (!removed) {
+        context.postTo(reply, $ -> $.response(NOT_FOUND, null));
+        return;
+      }
+      context.postTo(reply, $ -> $.response(OK, null));
+    }
+  }
+
+  private static TaskController taskController(Context context) {
+    var tasks = new ArrayList<Task>();
+    // initial task
+    tasks.add(new Task(0, "Hello from an http server powered by loom !"));
+    return new TaskController(context, tasks);
+  }
+
+  public static void main(String[] args) throws IOException, InterruptedException {
     var actor = Actor.of(TaskController.class)
-        .behavior(context -> new TaskController() {
-          private final ArrayList<Task> tasks = new ArrayList<>();
-          {
-            tasks.add(new Task(0, "Hello from an http server powered by loom !"));
-          }
-
-          @Override
-          public void getAllTasks(Actor<Response<List<Task>>> reply) {
-            System.err.println("getAllTasks");
-            context.postTo(reply, $ -> $.response(OK, tasks));
-          }
-
-          @Override
-          public void createTask(TaskWithoutId taskWithoutId, Actor<Response<Task>> reply) {
-            System.err.println("createTask " + taskWithoutId);
-            var task = new Task(tasks.size(), taskWithoutId.content);
-            tasks.add(task);
-            context.postTo(reply, $ -> $.response(OK, task));
-          }
-
-          @Override
-          public void deleteTask(String id, Actor<Response<Void>> reply) {
-            System.err.println("deleteTask " + id);
-            var taskId = Integer.parseInt(id);
-            var removed = tasks.removeIf(task -> task.id == taskId);
-            if (!removed) {
-              context.postTo(reply, $ -> $.response(NOT_FOUND, null));
-              return;
-            }
-            context.postTo(reply, $ -> $.response(OK, null));
-          }
-        })
+        .behavior(HttpMain::taskController)
         .onSignal((signal, context) -> {
           context.restart();  // restart if an error occurs
         });
