00ceb50e117f7909d0047798f130f649e90644d6
forax <forax@univ-mlv.fr>
2021-12-06 08:19:10 +0100

    move everything in the main()

--- src/main/java/fr/umlv/loom/actor/http/HttpMain.java
+++ src/main/java/fr/umlv/loom/actor/http/HttpMain.java
@@ -18,47 +18,47 @@ import static fr.umlv.loom.actor.http.HttpServer.RequestMethod.DELETE;
 import static fr.umlv.loom.actor.http.HttpServer.RequestMethod.POST;
 
 public class HttpMain {
-  record Task(long id, String content) {}
-  record TaskWithoutId(String content) {}
-
-  record TaskController(Context context, List<Task> tasks) {
-    @RequestMapping(path = "/tasks")
-    public void getAllTasks(Actor<Response<List<Task>>> reply) {
-      System.err.println("getAllTasks");
-      context.postTo(reply, $ -> $.response(OK, tasks));
-    }
-
-    @RequestMapping(path = "/tasks", method = POST)
-    public void createTask(@RequestBody TaskWithoutId taskWithoutId, Actor<Response<Task>> reply) {
-      System.err.println("createTask " + taskWithoutId);
-      var task = new Task(tasks.size(), taskWithoutId.content);
-      tasks.add(task);
-      context.postTo(reply, $ -> $.response(OK, task));
-    }
-
-    @RequestMapping(path = "/tasks/{id}", method = DELETE)
-    public void deleteTask(@PathVariable("id") String id, Actor<Response<Void>> reply) {
-      System.err.println("deleteTask " + id);
-      var taskId = Integer.parseInt(id);
-      var removed = tasks.removeIf(task -> task.id == taskId);
-      if (!removed) {
-        context.postTo(reply, $ -> $.response(NOT_FOUND, null));
-        return;
-      }
-      context.postTo(reply, $ -> $.response(OK, null));
-    }
-  }
-
-  private static TaskController taskController(Context context) {
-    var tasks = new ArrayList<Task>();
-    // initial task
-    tasks.add(new Task(0, "Hello from an http server powered by loom !"));
-    return new TaskController(context, tasks);
-  }
-
   public static void main(String[] args) throws IOException, InterruptedException {
+    record Task(long id, String content) {}
+    record TaskWithoutId(String content) {}
+
+    record TaskController(Context context, List<Task> tasks) {
+      public static TaskController behavior(Context context) {
+        var tasks = new ArrayList<Task>();
+        // initial task
+        tasks.add(new Task(0, "Hello from an http server powered by loom !"));
+        return new TaskController(context, tasks);
+      }
+
+      @RequestMapping(path = "/tasks")
+      public void getAllTasks(Actor<Response<List<Task>>> reply) {
+        System.err.println("getAllTasks");
+        context.postTo(reply, $ -> $.response(OK, tasks));
+      }
+
+      @RequestMapping(path = "/tasks", method = POST)
+      public void createTask(@RequestBody TaskWithoutId taskWithoutId, Actor<Response<Task>> reply) {
+        System.err.println("createTask " + taskWithoutId);
+        var task = new Task(tasks.size(), taskWithoutId.content);
+        tasks.add(task);
+        context.postTo(reply, $ -> $.response(OK, task));
+      }
+
+      @RequestMapping(path = "/tasks/{id}", method = DELETE)
+      public void deleteTask(@PathVariable("id") String id, Actor<Response<Void>> reply) {
+        System.err.println("deleteTask " + id);
+        var taskId = Integer.parseInt(id);
+        var removed = tasks.removeIf(task -> task.id == taskId);
+        if (!removed) {
+          context.postTo(reply, $ -> $.response(NOT_FOUND, null));
+          return;
+        }
+        context.postTo(reply, $ -> $.response(OK, null));
+      }
+    }
+
     var actor = Actor.of(TaskController.class)
-        .behavior(HttpMain::taskController)
+        .behavior(TaskController::behavior)
         .onSignal((signal, context) -> {
           context.restart();  // restart if an error occurs
         });
