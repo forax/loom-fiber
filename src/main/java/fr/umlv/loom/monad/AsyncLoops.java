8e2c6416e49b0e277a36e8cbf27bfe9aa9af34bb
forax <forax@univ-mlv.fr>
2022-04-23 19:03:49 +0200

    use monad exception handler

--- src/main/java/fr/umlv/loom/monad/AsyncLoops.java
+++ src/main/java/fr/umlv/loom/monad/AsyncLoops.java
@@ -1,6 +1,6 @@
 package fr.umlv.loom.monad;
 
-import fr.umlv.loom.monad.AsyncScope.ExceptionHandler;
+import fr.umlv.loom.monad.AsyncMonad.ExceptionHandler;
 
 import java.time.Duration;
 import java.time.Instant;
