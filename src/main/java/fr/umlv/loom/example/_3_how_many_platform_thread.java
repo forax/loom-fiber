913c640b45155a5ac72a1af724559b9779ebd753
forax <forax@univ-mlv.fr>
2022-06-13 11:43:05 +0200

    update example 3 and 4 to use a CountDownLatch

--- src/main/java/fr/umlv/loom/example/_3_how_many_platform_thread.java
+++ src/main/java/fr/umlv/loom/example/_3_how_many_platform_thread.java
@@ -1,6 +1,7 @@
 package fr.umlv.loom.example;
 
 import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.CyclicBarrier;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.IntStream;
@@ -8,11 +9,13 @@ import java.util.stream.IntStream;
 // $JAVA_HOME/bin/java -cp target/loom-1.0-SNAPSHOT.jar  fr.umlv.loom.example._3_how_many_platform_thread
 public interface _3_how_many_platform_thread {
   static void main(String[] args) throws BrokenBarrierException, InterruptedException {
+    var barrier = new CyclicBarrier(100_000);
     var threads = IntStream.range(0, 100_000)
         .mapToObj(i -> new Thread(() -> {
           try {
-            Thread.sleep(5_000);
-          } catch (InterruptedException e) {
+            //Thread.sleep(5_000);
+            barrier.await();
+          } catch (InterruptedException | BrokenBarrierException e) {
             throw new AssertionError(e);
           }
         }))
--- src/main/java/fr/umlv/loom/example/_4_how_many_virtual_thread.java
+++ src/main/java/fr/umlv/loom/example/_4_how_many_virtual_thread.java
@@ -1,5 +1,7 @@
 package fr.umlv.loom.example;
 
+import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.CyclicBarrier;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.IntStream;
 
@@ -7,11 +9,13 @@ import java.util.stream.IntStream;
 public interface _4_how_many_virtual_thread {
   static void main(String[] args) throws InterruptedException {
     var counter = new AtomicInteger();
+    var barrier = new CyclicBarrier(100_000);
     var threads = IntStream.range(0, 100_000)
         .mapToObj(i -> Thread.ofVirtual().unstarted(() -> {
           try {
-            Thread.sleep(5_000);
-          } catch (InterruptedException e) {
+            //Thread.sleep(5_000);
+            barrier.await();
+          } catch (InterruptedException | BrokenBarrierException e) {
             throw new AssertionError(e);
           }
           counter.incrementAndGet();
