3e43634c0dfef01a18c12f47b29f0f673fca908d
forax <forax@univ-mlv.fr>
2023-01-06 14:39:08 +0100

    auto-canceling

--- src/main/java/fr/umlv/loom/reducer/StructuredAsyncScope.java
+++ src/main/java/fr/umlv/loom/reducer/StructuredAsyncScope.java
@@ -144,8 +144,9 @@ public final class StructuredAsyncScope<T, A, V> extends StructuredTaskScope<T> 
     Result<T> result;
     switch (future.state()) {
       case CANCELLED -> {
-        // TODO automatic cancelling of the other tasks
-        // if (!shutdown) { shutdown(); }
+        // automatic cancelling of the other tasks
+        // this is a no-op if the scope is already shutdown
+        shutdown();
         return;
       }
       case SUCCESS -> result = new Result<>(State.SUCCEED, future.resultNow(), null);
