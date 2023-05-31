dba614d78bf5e0a71bc1deae7673305618f40a67
forax <forax@univ-mlv.fr>
2023-05-31 16:37:56 +0200

    declare an IOException to explain the exception propagation

--- src/main/java/fr/umlv/loom/example/_21_shutdown_on_success.java
+++ src/main/java/fr/umlv/loom/example/_21_shutdown_on_success.java
@@ -2,14 +2,15 @@ package fr.umlv.loom.example;
 
 import fr.umlv.loom.structured.StructuredScopeShutdownOnSuccess;
 
+import java.io.IOException;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.StructuredTaskScope;
 
 // $JAVA_HOME/bin/java --enable-preview -cp target/classes fr.umlv.loom.example._21_shutdown_on_success
 // docker run -it --rm --user forax -v /Users/forax:/home/forax -w /home/forax/git/loom-fiber fedora $JAVA_HOME/bin/java --enable-preview -cp target/classes fr.umlv.loom.example._21_shutdown_on_success
 public interface _21_shutdown_on_success {
-  static void main(String[] args) throws InterruptedException {
-    try (var scope = new StructuredScopeShutdownOnSuccess<Integer, RuntimeException>()) {
+  static void main(String[] args) throws IOException, InterruptedException {
+    try (var scope = new StructuredScopeShutdownOnSuccess<Integer, IOException>()) {
       scope.fork(() -> {
         Thread.sleep(1_000);
         return 1;
