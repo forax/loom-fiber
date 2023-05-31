11c51a521ab32d93d7129dc010e4e5d14e2f3e6b
forax <forax@univ-mlv.fr>
2023-05-31 17:10:16 +0200

    change the example to use summingInt

--- src/main/java/fr/umlv/loom/example/_23_as_stream.java
+++ src/main/java/fr/umlv/loom/example/_23_as_stream.java
@@ -1,6 +1,7 @@
 package fr.umlv.loom.example;
 
 import fr.umlv.loom.structured.StructuredScopeAsStream;
+import fr.umlv.loom.structured.StructuredScopeAsStream.Result;
 
 import java.io.IOException;
 import java.util.stream.Collectors;
@@ -8,7 +9,7 @@ import java.util.stream.Collectors;
 // $JAVA_HOME/bin/java --enable-preview -cp target/classes  fr.umlv.loom.example._23_as_stream
 // docker run -it --rm --user forax -v /Users/forax:/home/forax -w /home/forax/git/loom-fiber fedora $JAVA_HOME/bin/java --enable-preview -cp target/classes fr.umlv.loom.example._23_as_stream
 public interface _23_as_stream {
-  static void main(String[] args) throws InterruptedException {
+  static void main(String[] args) throws /*IOException,*/ InterruptedException {
     try (var scope = new StructuredScopeAsStream<Integer, IOException>()) {
       var task1 = scope.fork(() -> {
         Thread.sleep(1_000);
@@ -22,8 +23,8 @@ public interface _23_as_stream {
       var list = scope.joinAll(stream -> stream.toList());
       System.out.println(list);
 
-      //var result = scope.joinAll(s -> s.collect(StructuredScopeAsStream.Result.toResult(Collectors.toList())));
-      //System.out.println(result);
+      //var result = scope.joinAll(s -> s.collect(Result.toResult(Collectors.summingInt(v -> v))));
+      //System.out.println(result.get());
     }
   }
 }
